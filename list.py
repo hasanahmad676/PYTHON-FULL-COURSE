900442c2434b25fb17e9095e8d45ee83086e662c
MD Hasan Ahmad <82166280+hasanahmad676@users.noreply.github.com>
2023-07-12 22:03:41 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,30 @@
+# l=[[[1,2],[3,4]],[[5,6],[7,8]]]
+# print(l)
+# print(l[1][1][0])
+
+# print(list('hasan'))
+# print(l[1])
+
+# l=[1,2,3,4,5,6]
+# l.append(7)
+# print(l)
+
+# l=[1,2,3,4,5,6]
+# l.extend([7,8,9])
+# print(l)
+
+# l=[1,2,3,4,5,6]
+# l.insert(3, 4)
+# print(l)
+
+# l=[1,2,3,4,5,6]
+# l[2]='h'
+# print(l)
+
+# find languages which start with letter p
+languages = ['java','python','php','c','javascript']
+# print([language for language in languages if languages.startswith('p')])
+
+# [language for language in languages if language.startswith('p')]
+
+print([language for language in languages if language.startswith('p')])
